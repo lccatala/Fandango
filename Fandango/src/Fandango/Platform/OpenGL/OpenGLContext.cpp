2857d8d5edf6390239827dbeec20538691edd876
lccatala <lccm1@alu.ua.es>
2019-09-26 12:54:10 +0200

    Added check for correct OpenGL version
    It now checks you have OpenGL >= 4.5

--- Fandango/src/Fandango/Platform/OpenGL/OpenGLContext.cpp
+++ Fandango/src/Fandango/Platform/OpenGL/OpenGLContext.cpp
@@ -20,6 +20,12 @@ namespace Fandango
 
 		FNDG_ENGINE_INFO("OpenGL Vendor:   {0}", glGetString(GL_VENDOR));
 		FNDG_ENGINE_INFO("OpenGL Renderer: {0}", glGetString(GL_RENDERER));
+
+		int majorVersion, minorVersion;
+		glGetIntegerv(GL_MAJOR_VERSION, &majorVersion);
+		glGetIntegerv(GL_MINOR_VERSION, &minorVersion);
+
+		FNDG_ENGINE_ASSERT(majorVersion >= 4 && minorVersion >= 5, "Fandango requires at least OpenGL version 4.5");
 	}
 
 	void OpenGLContext::SwapBuffers()
